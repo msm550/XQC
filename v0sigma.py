8c3b1c96f6fe0fe8e304cc4f420511f45a605be7
msm550 <msm550@nyu.edu>
2017-03-22 21:16:19 -0400

    1. optimizing to use reasonable amount of memory

--- v0sigma.py
+++ v0sigma.py
@@ -89,62 +89,70 @@ def a_sel(ra, pASi, pATe):
         return (Te)
 
 
-def O(i):
-    return (bin[i][2])
-
-
-def E(i):
-    nSi = nHg = nTe = 0
-    if i == 0:
-        fe = 0.3815
-    elif i == 1:
-        fe = 0.5083
-    else:
-        fe = 1
-    sSi = []
-    sHg = []
-    sTe = []
-    for j in range(length):
-        if save[j][4] >= bin[i][0] and save[j][4] < bin[i][1]:
-            if save[j][5] == Si:
-                nSi += 1
-                sSi.append(save[j])
-            elif save[j][5] == Hg:
-                nHg += 1
-                sHg.append(save[j])
-            else:
-                nTe += 1
-                sTe.append(save[j])
-    sigmavSi = sum(sSi[i][0] * sSi[i][3] for i in range(nSi))
-    sigmavHg = sum(sHg[i][0] * sHg[i][3] for i in range(nHg))
-    sigmavTe = sum(sTe[i][0] * sTe[i][3] for i in range(nTe))
-    return (3e+7 * fr * eTime * N0 * fe * rhoDM / nj / mH * (
-        sigmavSi * mSi / Si / pASi + sigmavHg * mHg / Hg / pAHg + sigmavTe * mTe / Te / pATe))
-
-
-def scattering(i):
-    v = 1
-    while v > vesc:
-        v = maxwell.rvs(scale=v0, size=1)[0]
-    else:
-        ra = r01()
-        A = a_sel(ra, pASi, pATe)
-        CosXiCM = rCos()
-        rec = rE(mH, A, CosXiCM, v)
-        if rec <= 4e-6 and rec >= 2.505e-6:
-            return (0)
-        elif rec <= 29e-9:
-            return (0)
+def bounds(v0):
+    eRec = 0
+    print("The spectrum calculation for v0=" + str(int(v0 * 300000)) + " km/s is initiated!")
+    l_inv_Si = lambdainv(mH, si0, Si, eRec)
+    l_inv_Hg = lambdainv(mH, si0, Hg, eRec)
+    l_inv_Te = lambdainv(mH, si0, Te, eRec)
+    leff = lambdaeff(l_inv_Si, l_inv_Hg, l_inv_Te)
+    pASi = pA(l_inv_Si, leff)
+    pATe = pA(l_inv_Te, leff)
+    pAHg = 1 - pASi - pATe
+    save = []
+    for i in range(int(1e+6)):
+        v = 1
+        while v > vesc:
+            v = maxwell.rvs(scale=v0, size=1)[0]
         else:
-            return ([v, mH, si0, si(mH, si0, A, rec), rec, A])
-
-
-def getKey(custom):
-    return (custom[0])
+            ra = r01()
+            A = a_sel(ra, pASi, pATe)
+            CosXiCM = rCos()
+            rec = rE(mH, A, CosXiCM, v)
+            if rec >= 4e-6:
+                save.append([v, mH, si0, si(mH, si0, A, rec), rec, A])
+            elif rec >= 29e-9 and rec <= 2.505e-6:
+                save.append([v, mH, si0, si(mH, si0, A, rec), rec, A])
+    length = len(save)
+    Ei = []
+    for i in range(13):
+        nSi = nHg = nTe = 0
+        if i == 0:
+            fe = 0.3815
+        elif i == 1:
+            fe = 0.5083
+        else:
+            fe = 1
+        sSi = []
+        sHg = []
+        sTe = []
+        for j in range(length):
+            if save[j][4] >= bin[i][0] and save[j][4] < bin[i][1]:
+                if save[j][5] == Si:
+                    nSi += 1
+                    sSi.append(save[j])
+                elif save[j][5] == Hg:
+                    nHg += 1
+                    sHg.append(save[j])
+                else:
+                    nTe += 1
+                    sTe.append(save[j])
+        sigmavSi = sum(sSi[i][0] * sSi[i][3] for i in range(nSi))
+        sigmavHg = sum(sHg[i][0] * sHg[i][3] for i in range(nHg))
+        sigmavTe = sum(sTe[i][0] * sTe[i][3] for i in range(nTe))
+        Ei.append(3e+7 * fr * eTime * N0 * fe * rhoDM / nj / mH * (
+            sigmavSi * mSi / Si / pASi + sigmavHg * mHg / Hg / pAHg + sigmavTe * mTe / Te / pATe))
+    scale = max(Ei)
+    return([300000 * v0, 5 * si0 / scale] + [5 * Ei[i] / scale for i in range(13)])
 
 
 if __name__ == '__main__':
     n_cores = int(input("# of cores = "))
+    shielding_scenario = str(input('body or earth shielding? '))
+    if shielding_scenario == 'body':
+        fr = (1 - 0.848) / 2
+    elif shielding_scenario == 'earth':
+        fr = (1 + 0.245) / 2
     Si = 28
     Hg = 200
     Te = 127
@@ -163,35 +171,13 @@ if __name__ == '__main__':
     aF = 0.52
     sF = 0.9
     N0 = 6.02 * 1e+26
-    savet = []
     bin = [[29e-9, 36e-9, 0], [36e-9, 128e-9, 11], [128e-9, 300e-9, 129], [300e-9, 540e-9, 80], [540e-9, 700e-9, 90],
            [700e-9, 800e-9, 32], [800e-9, 945e-9, 48], [945e-9, 1100e-9, 31], [1100e-9, 1310e-9, 30],
            [1310e-9, 1500e-9, 29], [1500e-9, 1810e-9, 32], [1810e-9, 2505e-9, 15], [4000e-9, 1, 60]]
     mH = float(input("DM mass in GeV="))
-    fr = (1 - .848) / 2
     si0 = 5.5e-31
     vesc = 584 / 300000
-    for i in range(28):
-        eRec = 0
-        v0 = (30 + 10 * i) / 300000
-        print("The spectrum calculation for v0=" + str(int(v0 * 300000)) + " km/s is initiated!")
-        l_inv_Si = lambdainv(mH, si0, Si, eRec)
-        l_inv_Hg = lambdainv(mH, si0, Hg, eRec)
-        l_inv_Te = lambdainv(mH, si0, Te, eRec)
-        leff = lambdaeff(l_inv_Si, l_inv_Hg, l_inv_Te)
-        pASi = pA(l_inv_Si, leff)
-        pATe = pA(l_inv_Te, leff)
-        pAHg = 1 - pASi - pATe
-        pool1 = mp.Pool(n_cores)
-        save = []
-        s = pool1.map(scattering, range(int(1e+6)))
-        for j in range(nj):
-            if s[j] != 0:
-                save.append(s[j])
-        length = len(save)
-        pool2 = mp.Pool(n_cores)
-        Ei = pool2.map(E, range(13))
-        scale = max(Ei)
-        savet.append([300000 * v0, 5 * si0 / scale] + [5 * Ei[i] / scale for i in range(13)])
-
-    np.savetxt('sigma-v0_body(mH=' + format(mH, '.1f') + ').dat', savet)
+    vpeak = [(30 + 10 * i) / 300000 for i in range(28)]
+    pool1 = mp.Pool(n_cores)
+    savet = pool1.map(bounds,vpeak)
+    np.savetxt('sigma-v0_' + shielding_scenario +'(mH=' + format(mH, '.1f') + ').dat', savet)
